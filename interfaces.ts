e9764f833cdbba37a931c8c0d86831263e7c0ea5
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-30 13:27:02 -0600

    optional values in interface

--- interfaces.ts
+++ interfaces.ts
@@ -13,11 +13,29 @@ interface Person {
     firstName :string,
     lastName :string,
 }
+interface Animal {
+    firstName :string,
+    lastName? :string,
+}
+// can make values optional
 
 
 function fullNameWithInterface(person :Person) :string{
 	return `${person.firstName} ${person.lastName} is a cool guy`;
 }
 
+const animalName = (animal :Animal) => {
+    return `My dog is named ${animal.firstName} ${animal.lastName}`
+}
+
+
+
 
 fullNameWithInterface(p); //much cleaner! especially if objects have many values
+
+
+let dog = {
+    firstName: 'Cinder'
+}
+
+animalName(dog)
