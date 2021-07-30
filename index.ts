ca11cf1c2cc209378e1cc61f7992e7c33a2e85f6
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-30 12:49:02 -0600

    types

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-export {};
 
 let message = "Welcome Back";
 
@@ -8,4 +7,18 @@ console.log(message);
 let x = 10;
 const y = 20
 
-let x = 30;
+// let x = 30; //error re declared
+
+// const title; //error, must be initialized with a value
+
+const title = 'TITLE'
+
+let isBeginner :boolean = false;
+
+let total :number = 0
+
+let name :string = 'Ethan'
+
+let sentence :string = `My name is ${name} I am a beginner in Typescript`
+
+console.log(sentence)
